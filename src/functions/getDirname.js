0aa6959c433d4876c91926bd690fe7a5077f7cc6
Anatoly <povly19995@gmail.com>
2025-05-18 02:42:53 +0300

    File deleted -  getDirname.js

--- src/functions/getDirname.js
+++
@@ -1,6 +0,0 @@
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-export default function getDirname(importMetaUrl) {
-  const __filename = fileURLToPath(importMetaUrl);
-  return dirname(__filename);
-}
